6677389dde4d90f5d4844128dfa90ad7a147c5a1
Evon Troy Alexander <evon.codes@gmail.com>
2025-04-08 09:27:04 -0400

    chore: refined project structure with html template to display patient data.

--- src/main/java/com/example/medclinic/controller/BillingController.java
+++ src/main/java/com/example/medclinic/controller/BillingController.java
@@ -1,4 +1,7 @@
 package com.example.medclinic.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class BillingController {
 }
--- src/main/java/com/example/medclinic/controller/PatientController.java
+++ src/main/java/com/example/medclinic/controller/PatientController.java
@@ -1,4 +1,18 @@
 package com.example.medclinic.controller;
 
+import com.example.medclinic.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class PatientController {
+
+    private final PatientService patientService;
+
+    @Autowired
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    // Add get mapping
 }
